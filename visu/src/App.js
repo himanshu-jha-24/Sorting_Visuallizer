05db6561860a8febf929858f9512d6e34cc637e7
Himanshu Jha <himanshu.jha.min19@itbhu.ac.in>
2021-07-20 23:09:56 +0530

    First Commit

---
+++ visu/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SortVisualizer from "./SortVisualizer/SortVisualizer";
+
+function App() {
+  return (
+    <div className="App">
+      <SortVisualizer />
+    </div>
+  );
+}
+
+export default App;
